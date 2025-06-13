3bbfb47b6a09dfaa4be210d94e93fc3bbc8154cd
QIQIQIQIQIQIQIQIQIQIQIQ <1758582087@qq.com>
2025-06-13 09:38:44 +0100

    Added CNN, Added features to MLP

--- src/models/CNN.py
+++ src/models/CNN.py
@@ -1,4 +1,34 @@
-import torch 
+import torch.nn as nn
+import torch.nn.functional as F
 
-class CNN(torch.nn.Module):
-    ...
+class CNN(nn.Module): # Just CNN layers
+    def __init__(self, in_channels, hidden, out_channels, kernel_sizes, strides, paddings, dropouts=0, training=True):
+        super(CNN, self).__init__()
+        self.training = training
+        self.structure = [in_channels] + hidden + [out_channels]
+        self.convs = nn.ModuleList([nn.Conv2d(self.structure[i], self.structure[i+1], kernel_size=kernel_sizes[i], stride=strides[i], padding=paddings[i]) for i in range(len(self.structure)-1)])
+        self.dropouts = [dropouts] * (len(self.convs) - 1) if isinstance(dropouts, (float, int)) else dropouts
+            
+    def forward(self, x):
+        for i, layer in enumerate(self.convs):
+            if i < len(self.convs) - 1:
+                x = F.dropout(F.relu_(layer(x)), p=self.dropouts[i], training=self.training) ## Again ask abt efficiency
+            else:
+                break
+        return layer(x)
+    
+    def __str__(self):
+        pass ## TBC 
+
+
+
+class ConvNet(nn.Module):
+    def __init__(self, in_channels, hidden, out_channels, kernel_sizes, strides, paddings, dropouts=0, training=True):
+        super(CNN, self).__init__()
+        self.training = training
+        self.structure = [in_channels] + hidden + [out_channels]
+        convs = [nn.Conv2d(self.structure[i], self.structure[i+1], kernel_size=kernel_sizes[i], stride=strides[i], padding=paddings[i]) for i in range(len(self.structure)-1)]
+        self.layers = nn.ModuleList(convs + [nn.Flatten()] + [nn.Linear])
+        self.dropouts = [dropouts] * (len(self.convs) - 1) if isinstance(dropouts, (float, int)) else dropouts
+
+        
--- src/models/MLP.py
+++ src/models/MLP.py
@@ -1,18 +1,46 @@
-import torch 
+import torch.nn as nn
+import torch.nn.functional as F
 
-class MLP(torch.nn.Module):
-    def __init__(self, structure):
+
+class MLP(nn.Module):
+    """
+    Multi-Layer Perceptron (MLP) with customizable dropout rates.
+    Args:
+        structure (list): A list of integers defining the number of neurons in each layer.
+        dropouts (float or list): Dropout probability for each layer except the last. 
+                                  If a float, applies the same dropout to all layers.
+                                  If a list, must match the number of layers minus one.
+        training (bool): Whether the model is in training mode (default: True).
+    """
+    def __init__(self, structure, dropouts=0, training=True):
         super(MLP, self).__init__()
-        self.linears = torch.nn.ModuleList([torch.nn.Linear(structure[i], structure[i+1]) for i in range(len(structure)-1)])
-        print(self.linears)
-
+        self.training = training
+        self.linears = nn.ModuleList([nn.Linear(structure[i], structure[i+1]) for i in range(len(structure)-1)])
+        
+        # check and set dropout probabilities
+        if isinstance(dropouts, float) or isinstance(dropouts, int):
+            self.dropouts = [dropouts] * (len(self.linears)-1)
+        elif isinstance(dropouts, list) and len(dropouts) != len(self.linears)-1:
+            raise ValueError("Dropout list length must match the number of layers minus one.")
+        elif not isinstance(dropouts, list):
+            raise TypeError("Dropouts probability must be a float or a list of floats.")
+        
     def forward(self, x):
         for i, layer in enumerate(self.linears):
-            if i < len(self.linears) - 1:
-                x = torch.nn.functional.selu_(layer(x))
+            if i < len(self.linears)-1:
+                x = F.dropout(F.selu_(layer(x)), p=self.dropouts[i], training=self.training) ## ask about efficuency compared to nn.dropout and nn.selu
             else:
-                x = layer(x)
-        return x
+                break
+        return layer(x)
+    
+    def __str__(self):
+        """
+        Returns a summary of the model's architecture.
+        """
+        summary = "MLP Architecture:\n"
+        for i, layer in enumerate(self.linears):
+            summary += f"Layer {i}: {layer}\n"
+        return summary ## complete 
 
 if __name__ == "__main__":
     mlp = MLP([4, 10, 10, 3])
