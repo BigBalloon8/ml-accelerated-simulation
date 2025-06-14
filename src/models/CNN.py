77b1d4440d0636063ee6a7700d70bcb8db8b98a9
QIQIQIQIQIQIQIQIQIQIQIQ <1758582087@qq.com>
2025-06-14 10:38:31 +0100

    Added config file for MLP

--- src/models/CNN.py
+++ src/models/CNN.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 class CNN(nn.Module): # Just CNN layers
     def __init__(self, in_channels, hidden, out_channels, kernel_sizes, strides, paddings, dropouts=0, training=True):
-        super(CNN, self).__init__()
+        super().__init__()
         self.training = training
         self.structure = [in_channels] + hidden + [out_channels]
         self.convs = nn.ModuleList([nn.Conv2d(self.structure[i], self.structure[i+1], kernel_size=kernel_sizes[i], stride=strides[i], padding=paddings[i]) for i in range(len(self.structure)-1)])
@@ -24,11 +24,11 @@ class CNN(nn.Module): # Just CNN layers
 
 class ConvNet(nn.Module):
     def __init__(self, in_channels, hidden, out_channels, kernel_sizes, strides, paddings, dropouts=0, training=True):
-        super(CNN, self).__init__()
+        super().__init__()
         self.training = training
         self.structure = [in_channels] + hidden + [out_channels]
-        convs = [nn.Conv2d(self.structure[i], self.structure[i+1], kernel_size=kernel_sizes[i], stride=strides[i], padding=paddings[i]) for i in range(len(self.structure)-1)]
-        self.layers = nn.ModuleList(convs + [nn.Flatten()] + [nn.Linear])
+        self.convs = nn.ModuleList([nn.Conv2d(self.structure[i], self.structure[i+1], kernel_size=kernel_sizes[i], stride=strides[i], padding=paddings[i]) for i in range(len(self.structure)-1)])
+        self.fcs = nn.ModuleList([nn.Flatten()nn.LazyLinear()])
         self.dropouts = [dropouts] * (len(self.convs) - 1) if isinstance(dropouts, (float, int)) else dropouts
 
         
