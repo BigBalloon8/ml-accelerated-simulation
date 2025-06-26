87dc142a9270bc91afcb4a4ceae7a216a28f76e0
QIQIQIQIQIQIQIQIQIQIQIQ <1758582087@qq.com>
2025-06-26 22:34:05 +0800

    Fixed padding bugs

---
+++ src/summary.py
@@ -0,0 +1,9 @@
+import torchsummary as ts
+from models import buildModel
+import json
+import torch
+
+with open("src/models/configs/fullmodels/uNet1.json", "r") as f:
+    configs = json.load(f)
+    unet = buildModel(configs)
+ts.summary(unet, depth=5, input_data=torch.rand(1,2, 64, 64))
