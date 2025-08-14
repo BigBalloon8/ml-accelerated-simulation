148bc3eefce69d52cb4fcf3d5c9179152b418c90
BigBalloon8 <raecd123@gmail.com>
2025-08-14 13:43:08 +0100

    added lambda opt testing

--- src/train_v3.py
+++ src/train_v3.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from tqdm import tqdm
 import safetensors.torch as st
@@ -11,11 +10,11 @@ import os
 from typing import Tuple
 import random
 import hashlib
-from functools import partial
 
-from data.dataloader import get_kolomogrov_flow_data_loader
+from data.dataloader import get_kolomogrov_flow_data_loader_train_only
 from models import buildModel
 from log import Logger
+from inference_testing import main as inference_main
 
 def hash_dict(x:dict):
     formated_string = "".join(sorted(json.dumps(x, sort_keys=True)))
@@ -29,12 +28,12 @@ def get_model(name:str, config_file, checkpoint_path, logger, new_run)-> Tuple[n
 
     model_base = buildModel(config)
     
-    if f"{name}_{hash_dict(config)}_seg.safetensors" in os.listdir(checkpoint_path) and not new_run:
-        print(f"Model Found in {checkpoint_path}: {name}_{hash_dict(config)}_seg.safetensors")
-        model_path = os.path.join(checkpoint_path, f"{name}_{hash_dict(config)}_seg.safetensors")
-        opt_path = os.path.join(checkpoint_path, f"{name}_ADAM_{hash_dict(config)}_seg.pt")
+    if f"{name}_{hash_dict(config)}.safetensors" in os.listdir(checkpoint_path) and not new_run:
+        print(f"Model Found in {checkpoint_path}: {name}_{hash_dict(config)}.safetensors")
+        model_path = os.path.join(checkpoint_path, f"{name}_{hash_dict(config)}.safetensors")
+        opt_path = os.path.join(checkpoint_path, f"{name}_ADAM_{hash_dict(config)}.pt")
         model_weights = st.load_file(model_path)
-        with open(os.path.join(checkpoint_path, f"{name}_{hash_dict(config)}_seg.json"), "r") as f:
+        with open(os.path.join(checkpoint_path, f"{name}_{hash_dict(config)}.json"), "r") as f:
             metadata = json.load(f)
         model_base.load_state_dict(model_weights)
         opt_state = torch.load(opt_path)
@@ -42,22 +41,36 @@ def get_model(name:str, config_file, checkpoint_path, logger, new_run)-> Tuple[n
         metadata = {"last_epoch":-1}
         opt_state = None
     return model_base, metadata, opt_state
-    
+
+def get_segment_model(name, config_file, checkpoint_path):
+    with open(config_file, "r") as f:
+        config = json.load(f)
+
+    model_base = buildModel(config)
+    model_path = os.path.join(checkpoint_path, f"{name}_{hash_dict(config)}_seg.safetensors")
+    model_weights = st.load_file(model_path)
+    model_base.load_state_dict(model_weights)
+    return model_base
         
 def save_model(model:nn.Module, opt:torch.optim.Optimizer, model_type, checkpoint_path, model_config, metadata=None):
     with open(model_config, "r") as f:
         config = json.load(f)
     metadata["model_config"] = config
-    model_path = os.path.join(checkpoint_path, f"{model_type}_{hash_dict(config)}_seg.safetensors")
-    opt_path = os.path.join(checkpoint_path, f"{model_type}_ADAM_{hash_dict(config)}_seg.pt")
-    with open(os.path.join(checkpoint_path, f"{model_type}_{hash_dict(config)}_seg.json"), "w") as f:
+    model_path = os.path.join(checkpoint_path, f"{model_type}_{hash_dict(config)}.safetensors")
+    opt_path = os.path.join(checkpoint_path, f"{model_type}_ADAM_{hash_dict(config)}.pt")
+    with open(os.path.join(checkpoint_path, f"{model_type}_{hash_dict(config)}.json"), "w") as f:
         json.dump(metadata, f)
     st.save_model(model, model_path)
     #print(opt.state_dict())
     torch.save(opt.state_dict(), opt_path)
+
+
+def get_mask(x, segment_model):
+    with torch.no_grad():
+        logits = torch.softmax(segment_model(x), dim=1)
+        return torch.argmax(logits, dim=1, keepdim=True).to(torch.bool)
     
-    
-def main(data_path, model_type, model_config, checkpoint_path, log_file, new_run, Lambda=0.0025):
+def main(data_path, model_type, model_config, checkpoint_path, log_file, new_run, seg_model_name, seg_model_config):
     torch.set_default_dtype(torch.float64)
     torch.manual_seed(2025)
     random.seed(2025)
@@ -71,12 +84,15 @@ def main(data_path, model_type, model_config, checkpoint_path, log_file, new_run
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    train_dataloader, validation_dataloader = get_kolomogrov_flow_data_loader(data_path, batchsize=local_batch_size)
+    train_dataloader = get_kolomogrov_flow_data_loader_train_only(data_path, batchsize=local_batch_size)
 
     model, metadata, opt_state = get_model(model_type, model_config, checkpoint_path, logger, new_run)
     model = model.to(device)
 
-    criterion = torch.vmap(torch.vmap(F.cross_entropy, in_dims=-1, out_dims=-1), in_dims=-1, out_dims=-1)
+    seg_model = get_segment_model(seg_model_name, seg_model_config, checkpoint_path)
+    seg_model.to(device)
+
+    criterion = nn.MSELoss()
 
     opt = torch.optim.Adam(model.parameters())
     if opt_state is not None:
@@ -86,7 +102,8 @@ def main(data_path, model_type, model_config, checkpoint_path, log_file, new_run
 
     #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR
 
-    scale = 0.004482923474868822
+    mask_stream = torch.cuda.Stream()
+    model_stream = torch.cuda.Stream()
 
     for e in range(metadata["last_epoch"]+1, EPOCHS):
         model.train()
@@ -94,48 +111,41 @@ def main(data_path, model_type, model_config, checkpoint_path, log_file, new_run
         with tqdm(total=len(train_dataloader)*local_batch_size,desc=f"Epoch {e+1} Training Loss: NaN") as pbar:
             for i, (coarse, dif) in enumerate(train_dataloader):
                 coarse, dif = coarse.to(device), dif.to(device)
-                dif_labels = (torch.norm(dif, dim=1)>Lambda/scale).to(torch.int64)
-                logits = model.forward(coarse)
-                loss = criterion(logits, dif_labels).mean()
+                with torch.cuda.stream(mask_stream):
+                    mask = get_mask(coarse, seg_model)
+                with torch.cuda.stream(model_stream):
+                    pred = model.forward(coarse)
+                torch.cuda.synchronize()
+                pred = torch.masked_select(pred, mask)
+                dif = torch.masked_select(dif, mask)
+                loss = criterion.forward(pred, dif)
                 loss.backward()
                 total_loss += loss.item()
                 #if (i+1)%gradient_accumulation_steps==0:
-                torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+                #torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
                 opt.step()
                 opt.zero_grad()
                 pbar.update(local_batch_size)
                 pbar.set_description(f"Epoch {e+1} Loss: {loss.item():.8f}")
         logger.log(f"Train Loss at Epoch {e+1}: {total_loss/(len(train_dataloader))}")
 
-        model.eval()
-        with torch.no_grad():
-            total_loss = 0
-            total_acc = 0
-            with tqdm(total=len(validation_dataloader)*local_batch_size,desc=f"Epoch {e+1} Validation Loss: NaN") as pbar:
-                for coarse, dif in validation_dataloader:
-                    coarse, dif = coarse.to(device), dif.to(device)
-                    dif_labels = (torch.norm(dif, dim=1)>0.50).to(torch.int64)
-                    logits = model.forward(coarse)
-                    loss = criterion(logits, dif_labels).mean()
-                    total_loss += loss.item()
-                    total_acc += (torch.argmax(logits, dim=1)==dif_labels).sum()/torch.numel(dif_labels)
+        save_model(model, opt, model_type, checkpoint_path, model_config, {"last_epoch":e})   
 
-                    pbar.update(local_batch_size)
-                    pbar.set_description(f"Epoch {e+1} Validation Loss: {loss.item():.8f}")
-        logger.log(f"Validation Loss at Epoch {e+1}: {total_loss/(len(validation_dataloader))}")
-        logger.log(f"Validation Accuracy at Epoch {e+1}: {(total_acc*100)/(len(validation_dataloader)):.4f}%")
-        scheduler.step(total_loss/(len(validation_dataloader)*local_batch_size))
+        with torch.inference_mode():
+            inference_main(model_type, model_config, checkpoint_path, log_file, False, seg_model_name, seg_model_config)
 
-        save_model(model, opt, model_type, checkpoint_path, model_config, {"last_epoch":e})    
+         
 
         
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser() 
     ap.add_argument("--data_path", default="../data/data.safetensors", help="The path of the training data")
-    ap.add_argument("--model_type", default="CNN", help="Model to train: [MLP, CNN, KAN, Transformer]")
+    ap.add_argument("--model_type", default="CNN", help="Model to train")
     ap.add_argument("--model_config", default="./model.config", help="path to model config")
     ap.add_argument("--checkpoint_path", default=".", help="path to model config")
     ap.add_argument("--log_file", default="/content/drive/MyDrive/logs/general.log", help="path to log file")
     ap.add_argument("--new_run", action="store_true")
+    ap.add_argument("--seg_model_name", default="segment_big", help="segment model name")
+    ap.add_argument("--seg_model_config", default="models/configs/fullmodels/segnet.json", help="segment model config")
     main(**ap.parse_args().__dict__)
